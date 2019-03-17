5b8993a91e3a56fd75f3cb28335d25f5ccf1463d
Dylan Just <dylan.just@ephox.com>
2019-03-17 16:38:20 +1000

    Exercise 1.elm.1 - FizzBuzz in Elm

--- workshop/elm/Main.elm
+++ workshop/elm/Main.elm
@@ -1,4 +1,6 @@
 import Browser as B
+import Basics exposing (modBy)
+import String
 import Html as H
 import Html.Attributes as HA
 import Html.Events as HE
@@ -6,30 +8,43 @@ import Platform.Cmd exposing (Cmd)
 import Platform.Sub exposing (Sub)
 
 -- This datatype describes the events that can happen in our UI.
-type Msg = Toggle | Reset
+type Msg = Click | Reset
 
 -- This is the model that drives the UI
-type alias Model = { on : Bool }
+type alias Model = { clicks : Int }
 
 -- We initialise the model to our starting value
 init : Model
-init  = { on = True }
+init  = { clicks = 0 }
+
+text : Int -> String
+text i = 
+  let fizz = modBy 3 i == 0
+      buzz = modBy 5 i == 0
+  in
+    if      i == 0       then "0"
+    else if fizz && buzz then "FizzBuzz"
+    else if fizz         then "Fizz"
+    else if buzz         then "Buzz"
+    else                      String.fromInt i
 
 -- A function from model -> Html 
 view : Model -> H.Html Msg
 view model = 
-  let msg = if model.on then "Hello" else "World"
+  let msg = text model.clicks
   in H.main_ []
     -- This button raises a toggle event that triggers an update
-    [ H.button [ HA.class "big-button", HE.onClick Toggle ] 
-      [H.text msg] ]
+    [H.button 
+      [HA.class "big-button", HE.onClick Click] 
+      [H.text msg] 
+    ]
 
 -- Our update takes in our Msg and returns a new model (and optional side effect)
 update : Msg -> Model -> Model
 update msg model =
   case msg of
     -- We pattern match our two cases for Msg and act accordingly
-    Toggle -> { model | on = not model.on }
+    Click -> { model | clicks = model.clicks + 1 }
     Reset -> init
 
 -- Wire everything together into our program.
